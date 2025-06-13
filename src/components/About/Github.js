c8267a9056fa6c7fb69bbb921f55220bb0be3f6b
Dixit1010 <dixit.kumar.ug21@nsut.ac.in>
2025-06-13 14:48:00 +0530

    updated files

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -1,7 +1,4 @@
-import React from "react";
-import GitHubCalendar from "react-github-calendar";
 import { Row } from "react-bootstrap";
-
 function Github() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
